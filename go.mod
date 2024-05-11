2020373a67f89fbd0efe3eec950953eda31fa75b
krukaleksander <kruk.aleksander@hotmail.com>
2024-05-11 12:33:28 +0200

    chore: read port from the env

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module reading_list
 
 go 1.18
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -3,14 +3,30 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load()
+
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
 	handleRoutes()
 
-	fmt.Println("Server is running on port 8080")
+	fmt.Printf("Server is running on port %s\n", port)
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		fmt.Println("Error starting server: %s\n", err)
 	}
 }
